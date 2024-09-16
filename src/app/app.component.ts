771436d8617fcde88c7c887a515f37bb67c989f4
pratap360 <pratapparui0360@gmail.com>
2024-09-16 23:09:26 +0530

    create the login and signup page and remove the old side nav and added images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavBarComponent } from "../components/side-nav-bar/side-nav-bar.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideNavBarComponent],
+  imports: [RouterOutlet, 
+    SideNavBarComponent,
+    LoginComponent, 
+    SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router , RouterLink} from '@angular/router';
 
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  // loginForm!: FormGroup;
+  hide: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+  }
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+
+  onLogin() {
+    if(!this.loginForm.valid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
 
 }
