dbfb6ba5b56c10f9344085908567c8d17c0ff8f0
pratap360 <pratapparui0360@gmail.com>
2024-09-18 01:09:24 +0530

    update the login and added the signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router , RouterLink} from '@angular/router';
+
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGoogle} from '@fortawesome/free-brands-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FontAwesomeModule
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  hide: boolean = false;
+  faGoogle = faGoogle;
+  faHeart = faHeart;
+  constructor(private fb: FormBuilder) {
+  }
+  ngOnInit(): void {
 
+  }
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+
+  onLogin() {
+    if(!this.loginForm.valid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
 }
