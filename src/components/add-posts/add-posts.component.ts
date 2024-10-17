e269e61161b6b59f7ea6c556f8e8f7b30939cffe
pratap360 <pratapparui0360@gmail.com>
2024-10-18 01:11:57 +0530

    minor ui change in add post

--- src/components/add-posts/add-posts.component.ts
+++ src/components/add-posts/add-posts.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject, model,signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, model,OnInit,signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {  MAT_DIALOG_DATA,  MatDialog,  MatDialogActions,  MatDialogClose, MatDialogContent,  MatDialogRef,  MatDialogTitle,} from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCard, MatCardActions, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardModule } from '@angular/material/card';
+import { UsersService } from '../../app/services/users/users.service';
 @Component({
   selector: 'app-add-posts',
   standalone: true,
@@ -22,13 +23,29 @@ import { MatCard, MatCardActions, MatCardContent, MatCardHeader } from '@angular
     MatCardHeader,
     MatCardContent,
     MatCardActions,
+    MatCardModule,
     MatIconModule
   ],
   templateUrl: './add-posts.component.html',
   styleUrl: './add-posts.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddPostsComponent {
+export class AddPostsComponent implements OnInit {
   canPost: boolean = true;
+
+  users: any[] = [];
+
+  constructor(private userService: UsersService) { }
+
+  ngOnInit(): void {
+    this.userService.getsuggestUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      }
+    });
+  }
 
 }
