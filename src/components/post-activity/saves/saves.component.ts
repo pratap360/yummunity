dfdcc44f9801e39fdbc16ba43d893a9a6cef4f13
pratap360 <pratapparui0360@gmail.com>
2024-10-25 21:52:49 +0530

    added the save snackbar

--- src/components/post-activity/saves/saves.component.ts
+++ src/components/post-activity/saves/saves.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-saves',
@@ -15,8 +16,17 @@ import { MatIconModule } from '@angular/material/icon';
 export class SavesComponent {
   saves = 0;
   saved = false;
+  durationInSeconds = 5;
+  private _snackBar = inject(MatSnackBar);
+  horizontalPosition: MatSnackBarHorizontalPosition = 'right';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
   toggleSave(): void {
     this.saved = !this.saved;
     this.saves = this.saved ? this.saves + 1 : this.saves - 1;
+    this._snackBar.open('Saved to your profile !!', 'SEE PROFILE', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+    });
   }
 }
