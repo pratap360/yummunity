04f56f498e39af7a8393e3354bf757aa7b42c6c8
pratap360 <pratapparui0360@gmail.com>
2024-10-26 17:24:13 +0530

    activity button fixed

--- src/components/post-activity/saves/saves.component.ts
+++ src/components/post-activity/saves/saves.component.ts
@@ -1,32 +1,48 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
-
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-saves',
   standalone: true,
-  imports: [
-    MatButtonModule,
-    MatIconModule
-  ],
+  imports: [CommonModule,MatButtonModule, MatIconModule],
   templateUrl: './saves.component.html',
-  styleUrl: './saves.component.css'
+  styleUrl: './saves.component.css',
 })
 export class SavesComponent {
   saves = 0;
   saved = false;
   durationInSeconds = 5;
+  private router = inject(Router);
   private _snackBar = inject(MatSnackBar);
   horizontalPosition: MatSnackBarHorizontalPosition = 'right';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
+
   toggleSave(): void {
     this.saved = !this.saved;
     this.saves = this.saved ? this.saves + 1 : this.saves - 1;
-    this._snackBar.open('Saved to your profile !!', 'SEE PROFILE', {
-      horizontalPosition: this.horizontalPosition,
-      verticalPosition: this.verticalPosition,
-      duration: this.durationInSeconds * 1000,
-    });
+    if (this.saved) {
+     const snackBarRef = this._snackBar.open('Saved to your profile !!', 'SEE PROFILE', {
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+        duration: this.durationInSeconds * 1000,
+      });
+      // Subscribe to the snackbar action to navigate to the profile page
+      snackBarRef.onAction().subscribe(() => {
+      this.router.navigate(['/account']);
+            });
+    } else {
+      this._snackBar.open('Removed from your profile !!', 'OK', {
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+        duration: this.durationInSeconds * 1000,
+      });
+    }
   }
 }
