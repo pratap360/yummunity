9de7984c4c950eead808336cb11b84d37ca7398b
pratap360 <pratapparui0360@gmail.com>
2024-10-28 00:43:20 +0530

    added serect api key

--- src/app/services/pexels-bg-api/pexels.service.ts
+++ src/app/services/pexels-bg-api/pexels.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 @Injectable({
   providedIn: 'root'
 })
 export class PexelsService {
   private apiKey = 'xzj5QnItQBnTeN6Un9iLqDHBltHCUQA1Vqwb0qs952Vk760lSptyTc8m';  
+  // private apiKey = process.env['PEXELS_API_KEY'] || '';
   private apiUrl = 'https://api.pexels.com/v1/search';
 
   constructor(private http: HttpClient) {}
@@ -15,6 +17,7 @@ export class PexelsService {
   getRandomFoodImage(): Observable<string | null> {
     const headers = new HttpHeaders({
       Authorization: this.apiKey
+      // Authorization: !!this.apiKey
     });
 
     return this.http.get<any>(this.apiUrl, {
