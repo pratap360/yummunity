237f5d395a96c47fa1b463dbeac490ad00be374f
pratap360 <pratapparui0360@gmail.com>
2025-02-15 19:02:35 +0530

    add edit and welcome components also added user-data interface

---
+++ src/app/interface/user-data.ts
@@ -0,0 +1,17 @@
+export interface UserData {
+  id?: string;
+  user_tag: string;
+  user_profile_pic?: any[];
+  user_name: string;
+  user_bio?: string;
+  user_email: string;
+  user_password: string;
+  user_phone_no: number;
+  user_gender: string;
+  user_location: string;
+  user_url: string;
+  user_fav_food_recipe: string;
+  yummunity_rating: number;
+  user_following_count: string[];
+  user_followers_count: string[];
+}
