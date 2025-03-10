bd78da9989dab3dbb4f68327317540404dc26381
pratap360 <pratapparui0360@gmail.com>
2025-03-10 22:25:11 +0530

    fixing the home-feed user post data

--- src/app/interface/blog-post.ts
+++ src/app/interface/blog-post.ts
@@ -5,6 +5,7 @@ export interface BlogPost {
   user_name: string | null;
   user_bio?: string | null;
   user_tag?: string | null;
+  user_profile_pic?: string | null;
   blog_post_title: string;
   blog_post_summary: string;
   blog_post_tags: string[];
